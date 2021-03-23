261b794d6373fe9c540084866cbf110e09f4f2f4
Nick Toye <nick@pixelsandcode.co.uk>
2021-03-23 22:11:59 +0000

    Changed the method of tracking keys used in array

--- components/Tabs.jsx
+++ components/Tabs.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import uuid from 'react-uuid';
 
 const tabList = ['Demographics', 'Climate', 'Culture', 'Economy'];
 
 export default function Tabs() {
   return (
     <div className="tabs">
-      {tabList.map((x, i) => <div key={i}>{x}</div>)}
+      {tabList.map((x) => <div key={uuid()}>{x}</div>)}
     </div>
   );
 }
