b3a57a073de671a4b03eacb0589e0b2816903aa7
Nick Toye <nick@pixelsandcode.co.uk>
2021-03-25 14:27:50 +0000

    implemented map and filter logic for the data stream

--- components/Tabs.jsx
+++ components/Tabs.jsx
@@ -1,32 +1,29 @@
 import React, { useState } from 'react';
 import uuid from 'react-uuid';
 
-const tabsList = ['Demographics', 'Climate', 'Culture', 'Economy'];
+export default function Tabs(props) {
+  const { data } = props;
+  const [activeTab, setActiveTab] = useState(data[0].id);
+  const activeContent = data.filter((x) => x.id === activeTab).map((x) => x.content);
 
-export default function Tabs() {
-  const [activeTab, setActiveTab] = useState(tabsList[0]);
   return (
     <>
-      <span>
-        Active Tab is
-        {' '}
-        {activeTab}
-      </span>
       <div className="tabs">
-        {tabsList.map((x, i) => (
+        {data.map((x, i) => (
           <div
             className="tabs_list"
             key={uuid()}
-            onClick={() => setActiveTab(x)}
-            onKeyDown={() => setActiveTab(x)}
+            onClick={() => setActiveTab(x.id)}
+            onKeyDown={() => setActiveTab(x.id)}
             role="button"
             tabIndex={i}
           >
-            {x}
-            {' '}
-            data
+            {x.label}
           </div>
         ))}
+      </div>
+      <div>
+        {activeContent}
       </div>
     </>
   );
